346bf9e101220c7e80b849a6b7e5658f9d75a755
«Egor <egoled8@gmail.com>
2022-06-13 16:08:41 +0300

    Задача 1  от 9го июня

---
+++ Exercise_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+void PrintArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine (array[1]);
+    }
+}
+Console.WriteLine("Введите трехзначное число: ");
+string Num = Console.ReadLine();
+int N = int.Parse(Num);
+
+int [] array = new int [3];
+array [0] = N / 100;
+array [1] = N % 100 /10;
+array [2] = N % 10;
+
+Console.WriteLine(array[1]);
