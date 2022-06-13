82fb7998a9331682e574c21254ef9657a7d406fc
«Egor <egoled8@gmail.com>
2022-06-13 16:57:29 +0300

    Задача 2  от 9го июня

---
+++ Exercise_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int RandNum = new Random(). Next(100, 999);
+Console.WriteLine("Случайное трехзначное число: " + RandNum);
+int One = RandNum / 100;
+int Two = RandNum % 100 /10;
+int Three = RandNum % 10;
+Console.WriteLine ("Удаляем вторую цифру: " + One + Three);
